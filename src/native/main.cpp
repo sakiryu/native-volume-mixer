63ea987589b17c3545d7e7d040bba991209d0783
Haise <sakiryu>
2024-07-29 00:03:34 +0200

    Cleanup main

--- src/native/main.cpp
+++ src/native/main.cpp
@@ -1,9 +1,7 @@
-#include "volume_mixer.h"
+#include "app.h"
 
 int main(int argc, char** argv)
 {
-	std::unique_ptr<IVolumeMixer> volume{ new VolumeMixer() };
-	volume->set_volume(0.3f); //30%
-	volume->set_volume(0.6f); //60%
-	volume->set_volume(0.9f); //90%
+	App app{};
+	app.run();
 }
