adda18672908e1ce83613b018f6167a6f0ffb79b
Haise <sakiryu>
2024-07-30 22:54:35 +0200

    Add option to install dependencies of electron project; Starts UI passing the path of the volume_mixer as arg

--- src/build_and_run.py
+++ src/build_and_run.py
@@ -29,20 +29,35 @@ def find_vs_dev_cmd():
 
     return None
 
-def build_project(solution_path, configuration):
+def build_cpp_project(solution_path, configuration, platform):
     vs_dev_cmd = find_vs_dev_cmd()
 
     if not vs_dev_cmd:
         print("Error: Could not find VsDevCmd.bat")
         sys.exit(1)
 
-    build_command = f'call "{vs_dev_cmd}" && msbuild "{solution_path}" /p:Configuration={configuration}'
+    build_command = f'call "{vs_dev_cmd}" && msbuild "{solution_path}" /p:Configuration={configuration} /p:Platform={platform}'
 
     try:
         result = subprocess.run(build_command, shell=True, check=True)
-        print("Build succeeded." if result.returncode == 0 else "Build failed.")
     except subprocess.CalledProcessError as e:
         print("Error during build:", e)
+        sys.exit(e.returncode)
+
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(project_dir, 'native', platform if platform  == 'x64' else '', configuration)
+
+    return build_dir
+
+def build_electron_project(executable_path):
+    os.chdir('ui')
+    try:
+        if not os.path.exists('node_modules'):
+            subprocess.check_call(['npm', 'install'], shell=True)
+        
+        subprocess.check_call(['npm', 'start', executable_path], shell=True)
+    except subprocess.CalledProcessError as e:
+        print("Electron build failed")
         sys.exit(e.returncode)
 
 if __name__ == "__main__":
@@ -55,10 +70,18 @@ if __name__ == "__main__":
         print("Error: Configuration must be either 'Debug' or 'Release'.")
         sys.exit(1)
 
+    platform = sys.argv[2]
+    if platform not in ("x86", "x64"):
+        print("Error: Platform must be either 'x86' or 'x64'.")
+        sys.exit(1)
+
     solution_file = "native/volume_mixer.sln"
     
     if not os.path.exists(solution_file):
         print(f"Error: Solution file {solution_file} does not exist.")
         sys.exit(1)
 
-    build_project(solution_file, configuration)
+    build_dir = build_cpp_project(solution_file, configuration, platform)
+    executable_path = os.path.join(build_dir, 'volume_mixer.exe')
+
+    build_electron_project(executable_path)
